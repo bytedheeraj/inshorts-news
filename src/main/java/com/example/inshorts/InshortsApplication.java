981d6db9a8885df81de9c353a985b0cfa3a389df
Dheeraj <dheeraj@Infinity.local>
2025-08-24 02:53:23 +0530

    Initial commit

---
+++ src/main/java/com/example/inshorts/InshortsApplication.java
@@ -0,0 +1,13 @@
+package com.example.inshorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InshortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InshortsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inshorts/InshortsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inshorts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InshortsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
